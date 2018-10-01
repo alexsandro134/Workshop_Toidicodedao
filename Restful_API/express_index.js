dc923bcce58b01d49b34fa679092b947fcdecb6c
AlexX Le <alexsandro134@gmail.com>
2018-10-01 18:29:02 +0700

    Update RestfulAPI

--- Restful_API/express_index.js
+++ Restful_API/express_index.js
@@ -3,8 +3,34 @@ var app = express();
 
 var port = 4000;
 
-app.get('/', function(req, res) {
-    res.send('Hello world!');
+app.set('views', './views') // specify the views directory
+app.set('view engine', 'pug')
+
+var articles = [
+    {id: 1, title: 'Bai bao so 1'},
+    {id: 2, title: 'Bai bao so 2'}
+];
+
+app.get('/', function (req, res) {
+    res.render('index.pug');
+});
+
+app.get('/api', function (req, res) {
+    res.render('api/index.pug', {
+        dataList: articles
+    });
+});
+
+app.get('/api/search', function (req, res) {
+    var q = req.query.q;
+    var matchedArticles = articles.filter(function (item) {
+        return item.title.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+
+    res.render('api/index.pug', {
+        searchText: q,
+        dataList: matchedArticles
+    });
 });
 
 app.listen(port, function () {
